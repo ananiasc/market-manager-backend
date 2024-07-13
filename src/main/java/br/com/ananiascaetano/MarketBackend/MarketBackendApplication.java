57514077f637e9df297304242df93f41af90850d
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-07-13 14:19:57 -0300

    Início de Projeto: Spring Boot, Java 1.8, Maven

---
+++ src/main/java/br/com/ananiascaetano/MarketBackend/MarketBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ananiascaetano.MarketBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ananiascaetano/MarketBackend/MarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ananiascaetano.MarketBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

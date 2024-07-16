3062471edb5c2f0569531ca90cc0f8d2b752e1ea
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-07-15 21:31:23 -0300

    fix: remove findByName of ProductTypeRepository

--- src/main/java/br/com/ananiascaetano/infrastructure/repositories/product/ProductTypeRepository.java
+++ src/main/java/br/com/ananiascaetano/infrastructure/repositories/product/ProductTypeRepository.java
@@ -1,11 +1,10 @@
 package br.com.ananiascaetano.infrastructure.repositories.product;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.ananiascaetano.domain.entities.product.ProductType;
 
 public interface ProductTypeRepository extends JpaRepository<ProductType, Integer> {
-	Optional<ProductType> findByName(String name);
+	
 }

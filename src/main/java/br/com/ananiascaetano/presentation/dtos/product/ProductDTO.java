95a12f4a7a3ecba83e4b4eabd3f94d2f6ec6e19d
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-07-15 21:33:01 -0300

    fix: removing unused functions

--- src/main/java/br/com/ananiascaetano/presentation/dtos/product/ProductDTO.java
+++ src/main/java/br/com/ananiascaetano/presentation/dtos/product/ProductDTO.java
@@ -2,10 +2,6 @@ package br.com.ananiascaetano.presentation.dtos.product;
 
 import java.math.BigDecimal;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import br.com.ananiascaetano.domain.entities.product.Product;
-import br.com.ananiascaetano.domain.entities.product.ProductType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,50 +22,4 @@ public class ProductDTO {
 	private int stock;
 	private Boolean isActive;
 	
-	public ProductDTO(Product product) {
-		this.id = product.getId();
-		this.code = product.getCode();
-		this.title = product.getTitle();
-		this.technicalDescription = product.getTechnicalDescription();
-		this.longDescription = product.getLongDescription();
-		this.price = product.getPrice();
-		this.discountedPrice = product.getDiscountedPrice();
-		this.isDiscounted = product.isDiscounted();
-		this.type = product.getType().getName();
-		this.stock = product.getStock();
-		this.isActive = product.isActive();
-	}
-	
-	public Product getProduct(ProductType type) {
-		Product product = Product.builder()
-						.code(this.code)
-						.title(this.title)
-						.technicalDescription(this.technicalDescription)
-						.longDescription(this.longDescription)
-						.price(this.price)
-						.discountedPrice(this.discountedPrice)
-						.isDiscounted(this.isDiscounted())
-						.type(type)
-						.stock(this.stock)
-						.isActive(this.isActive())
-						.build();
-		
-		return product;
-	}
-	
-	@JsonIgnore
-	public boolean isDiscounted() {
-		if(this.isDiscounted != null && this.isDiscounted) {
-			return true;
-		}
-		return false;
-	}
-	
-	@JsonIgnore
-	public boolean isActive() {
-		if(this.isActive != null && this.isActive) {
-			return true;
-		}
-		return false;
-	}
 }

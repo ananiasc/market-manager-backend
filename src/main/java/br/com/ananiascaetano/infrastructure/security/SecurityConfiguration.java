3a304561366d616734be831d1d69d6443b3ea0ef
Ananias Caetano <ananiascaetano.ifce@gmail.com>
2024-08-18 13:44:10 -0300

    feat: allows request of all routes

--- src/main/java/br/com/ananiascaetano/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/br/com/ananiascaetano/infrastructure/security/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.cors.CorsConfiguration;
 
 @RequiredArgsConstructor
 @Configuration
@@ -25,6 +26,8 @@ public class SecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		return httpSecurity
+				.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+				.and()
 				.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(

973b1ff64b0fec7c6eb66e88a18b4910287acc84
Ananias Caetano <ananiascaetano556@gmail.com>
2024-08-29 22:17:18 -0300

    feat: creating custom callback for login error

--- src/main/java/br/com/ananiascaetano/constants/ErrorMessages.java
+++ src/main/java/br/com/ananiascaetano/constants/ErrorMessages.java
@@ -4,4 +4,5 @@ public final class ErrorMessages {
 	public static final String PRODUCT_TYPE_NOT_FOUND = "Product type not found";
 	public static final String JWT_GENERATE_TOKEN_FAIL = "Error while generating token";
 	public static final String JWT_VERIFICATION_TOKEN_FAIL = "Error while verifying token";
+	public static final String AUTHENTICATION_FAIL = "Usuário inexistente ou senha inválida";
 }

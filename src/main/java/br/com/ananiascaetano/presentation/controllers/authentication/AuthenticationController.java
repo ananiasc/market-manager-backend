0475d91ded2773b462b95b429752eecaa08b93f7
Ananias Caetano <ananiascaetano556@gmail.com>
2024-08-31 22:37:12 -0300

    remove: removing import unused

--- src/main/java/br/com/ananiascaetano/presentation/controllers/authentication/AuthenticationController.java
+++ src/main/java/br/com/ananiascaetano/presentation/controllers/authentication/AuthenticationController.java
@@ -1,6 +1,5 @@
 package br.com.ananiascaetano.presentation.controllers.authentication;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;

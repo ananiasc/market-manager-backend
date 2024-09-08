c187b37dfb8b572c8c8f7707bd2fc5c05e958305
Ananias Caetano <ananiascaetano556@gmail.com>
2024-09-07 22:51:25 -0300

    feat: implements UserDetails

--- src/main/java/br/com/ananiascaetano/domain/entities/user/UserDetailsImpl.java
+++ src/main/java/br/com/ananiascaetano/domain/entities/user/UserDetailsImpl.java
@@ -1,4 +1,60 @@
 package br.com.ananiascaetano.domain.entities.user;
 
-public class UserDetailsImpl {
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+public class UserDetailsImpl  implements UserDetails {
+    private Long id;
+    private String username;
+    private String password;
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN) {
+            return Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        }
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return true;
+    }
 }

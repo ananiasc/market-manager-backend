1ab4c1dbc52aeabbbd96fba44e313a834483ec79
Ananias Caetano <ananiascaetano556@gmail.com>
2024-09-25 23:29:27 -0300

    feat: creating delete methid for product

--- src/main/java/br/com/ananiascaetano/presentation/controllers/product/ProductController.java
+++ src/main/java/br/com/ananiascaetano/presentation/controllers/product/ProductController.java
@@ -3,6 +3,8 @@ package br.com.ananiascaetano.presentation.controllers.product;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import br.com.ananiascaetano.application.services.product.ProductService;
@@ -10,6 +12,8 @@ import br.com.ananiascaetano.domain.entities.product.Product;
 import br.com.ananiascaetano.mappers.product.ProductMapper;
 import br.com.ananiascaetano.presentation.dtos.product.ProductDTO;
 import lombok.RequiredArgsConstructor;
+
+import javax.xml.ws.Response;
 
 @RequiredArgsConstructor
 @RestController
@@ -41,5 +45,11 @@ public class ProductController {
 		Product product = this.model.map(productDTO, Product.class);
 		Product updatedProduct =  productService.updateProduct(product);
 		return this.model.map(updatedProduct, ProductDTO.class);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+		productService.deleteProduct(id);
+		return new ResponseEntity<>("Deleted user", HttpStatus.OK);
 	}
 }

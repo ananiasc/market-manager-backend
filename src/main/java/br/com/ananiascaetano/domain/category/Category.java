63abc056ed7b6828b934f6ded5aebc15038ef4bf
Ananias Caetano <ananiascaetano556@gmail.com>
2025-05-01 16:26:09 -0300

    fix: defining id

--- src/main/java/br/com/ananiascaetano/domain/category/Category.java
+++ src/main/java/br/com/ananiascaetano/domain/category/Category.java
@@ -2,6 +2,9 @@ package br.com.ananiascaetano.domain.category;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -16,6 +19,8 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(of = "id")
 @Table(name = "categories")
 public class Category {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     @Column(name = "parent_category_id")

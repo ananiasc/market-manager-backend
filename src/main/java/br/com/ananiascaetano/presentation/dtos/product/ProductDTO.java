f2fd8a7d8802a56bca1bfa365ebe95841ae64beb
Ananias Caetano <ananiascaetano556@gmail.com>
2025-05-04 18:08:22 -0300

    feat: defining attribute validations

--- src/main/java/br/com/ananiascaetano/presentation/dtos/product/ProductDTO.java
+++ src/main/java/br/com/ananiascaetano/presentation/dtos/product/ProductDTO.java
@@ -3,6 +3,10 @@ package br.com.ananiascaetano.presentation.dtos.product;
 import java.math.BigDecimal;
 import java.util.List;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import br.com.ananiascaetano.constants.ValidationMessages;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,18 +15,31 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDTO {
-	private Long id;
+	@NotNull(message = ValidationMessages.ATTRIBUTE_IS_NOT_NULL)
 	private String code;
+	@NotNull(message = ValidationMessages.ATTRIBUTE_IS_NOT_NULL)
 	private String title;
+	@NotNull(message = ValidationMessages.ATTRIBUTE_IS_NOT_NULL)
 	private String technicalDescription;
+	@NotNull(message = ValidationMessages.ATTRIBUTE_IS_NOT_NULL)
 	private String longDescription;
+	@NotNull(message = ValidationMessages.ATTRIBUTE_IS_NOT_NULL)
 	private BigDecimal price;
+	@NotNull(message = ValidationMessages.ATTRIBUTE_IS_NOT_NULL)
 	private BigDecimal discountedPrice;
+	@NotNull(message = ValidationMessages.ATTRIBUTE_IS_NOT_NULL)
 	private boolean discounted;
+	@NotNull(message = ValidationMessages.ATTRIBUTE_IS_NOT_NULL)
 	private Integer typeId;
+	@NotNull(message = ValidationMessages.ATTRIBUTE_IS_NOT_NULL)
 	private int stock;
+	@NotNull(message = ValidationMessages.ATTRIBUTE_IS_NOT_NULL)
 	private boolean active;
+	@NotNull(message = ValidationMessages.ATTRIBUTE_IS_NOT_NULL)
 	private String slug;
+	@NotNull(message = ValidationMessages.ATTRIBUTE_IS_NOT_NULL)
 	private Integer brandId;
+	@NotNull(message = ValidationMessages.ATTRIBUTE_IS_NOT_NULL)
+	@NotEmpty(message = ValidationMessages.LIST_IS_NOT_EMPTY)
 	private List<CategoryDTO> categories;
 }

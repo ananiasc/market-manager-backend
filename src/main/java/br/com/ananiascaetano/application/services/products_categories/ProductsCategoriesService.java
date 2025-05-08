bf464f446ebbdb21edad0a71a5dc89be0a0babce
Ananias Caetano <ananiascaetano556@gmail.com>
2025-05-07 23:10:38 -0300

    feat: creating ProductsCategoriesService

---
+++ src/main/java/br/com/ananiascaetano/application/services/products_categories/ProductsCategoriesService.java
@@ -0,0 +1,11 @@
+package br.com.ananiascaetano.application.services.products_categories;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductsCategoriesService {
+
+}
